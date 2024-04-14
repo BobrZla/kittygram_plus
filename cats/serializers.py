5822f7ff09fb1d33a4989388eb172937724ec1aa
Dmitry <bobrzla@yandex.ru>
2024-04-14 18:18:46 +0300

    save

--- cats/serializers.py
+++ cats/serializers.py
@@ -1,23 +1,56 @@
 # cats/serializers.py
 from rest_framework import serializers
+import datetime as dt
+import webcolors
 
-from .models import Cat, Owner, Achievement, AchievementCat
+
+
+from .models import Cat, Owner, Achievement, AchievementCat, CHOICES
+
+
+class Hex2NameColor(serializers.Field):
+    # При чтении данных ничего не меняем - просто возвращаем как есть
+    def to_representation(self, value):
+        return value
+    # При записи код цвета конвертируется в его название
+    def to_internal_value(self, data):
+        # Доверяй, но проверяй
+        try:
+            # Если имя цвета существует, то конвертируем код в название
+            data = webcolors.hex_to_name(data)
+        except ValueError:
+            # Иначе возвращаем ошибку
+            raise serializers.ValidationError('Для этого цвета нет имени')
+        # Возвращаем данные в новом формате
+        return data
 
 
 class AchievementSerializer(serializers.ModelSerializer):
+    achievement_name = serializers.CharField(source='name')
 
     class Meta:
         model = Achievement
         fields = ('id', 'name')
 
 
+class CatListSerializer(serializers.ModelSerializer):
+    color = serializers.ChoiceField(choices=CHOICES)
+    
+    class Meta:
+        model = Cat
+        fields = ('id', 'name', 'color')
+
+
 class CatSerializer(serializers.ModelSerializer):
-    achievements = AchievementSerializer(many=True, required=False)  # Убрали read_only=True
+    achievements = AchievementSerializer(many=True, required=False)
+    age = serializers.SerializerMethodField()
+    # color = Hex2NameColor(choices=CHOICES)
+    # Убрали read_only=True
     # Убрали owner = serializers.StringRelatedField(read_only=True)
 
     class Meta:
         model = Cat
-        fields = ('id', 'name', 'color', 'birth_year', 'owner', 'achievements')
+        fields = ('id', 'name', 'color', 'birth_year', 'owner', 'achievements', 'age')
 
     def create(self, validated_data):
         # Если в исходном запросе не было поля achievements
@@ -25,6 +58,9 @@ class CatSerializer(serializers.ModelSerializer):
             # То создаём запись о котике без его достижений
             cat = Cat.objects.create(**validated_data)
             return cat
+
+    def get_age(self, obj):
+        return dt.datetime.now().year - obj.birth_year
 
         # Иначе делаем следующее:
         # Уберём список достижений из словаря validated_data и сохраним его
--- kittygram_plus/urls.py
+++ kittygram_plus/urls.py
@@ -1,15 +1,21 @@
 # kittygram_plus/urls.py
 from rest_framework.routers import DefaultRouter
+# from rest_framework.authtoken import views
 
 from django.urls import include, path
 
-from cats.views import CatViewSet, OwnerViewSet
+from cats.views import CatViewSet, OwnerViewSet, LightCatViewSet
 
 
 router = DefaultRouter()
-router.register('cats', CatViewSet)
-router.register('owners', OwnerViewSet)
+router.register('cats', CatViewSet, basename='cats')
+router.register('owners', OwnerViewSet, basename='owners')
+router.register(r'mycats', LightCatViewSet, basename='mycats') 
+
 
 urlpatterns = [
     path('', include(router.urls)),
+    # path('api-token-auth/', views.obtain_auth_token),
+    path('auth/', include('djoser.urls')),
+    path('auth/', include('djoser.urls.jwt')),
 ]
